dc610ec034a51979d1802c5bdee86c7483528708
griffindunn <griffindunn@gmail.com>
2026-02-13 13:43:42 -0800

    Update QueueLookupDetails.jsx

--- src/components/details/QueueLookupDetails.jsx
+++ src/components/details/QueueLookupDetails.jsx
@@ -5,14 +5,8 @@ const QueueLookupDetails = ({ details }) => (
   <>
     <div style={s.section}>
       <div style={s.sectionTitle}>Lookup Settings</div>
-      <div style={s.row}>
-         <span style={s.label}>Lookback Period</span>
-         <span style={s.value}>{details.ewtLookbackMinutes} minutes</span>
-      </div>
-      <div style={s.row}>
-         <span style={s.label}>Queue ID/Name</span>
-         <span style={s.value}>{details.destination || details['destination:name']}</span>
-      </div>
+      <div style={s.row}><span style={s.label}>Lookback</span><span style={s.value}>{details.ewtLookbackMinutes} minutes</span></div>
+      <div style={s.row}><span style={s.label}>Queue</span><span style={s.value}>{details.destination || details['destination:name']}</span></div>
     </div>
   </>
 );
