581275b4dbe9bd5e9579088708d867a874400047
griffindunn <griffindunn@gmail.com>
2026-02-13 13:43:57 -0800

    Update TransferDetails.jsx

--- src/components/details/TransferDetails.jsx
+++ src/components/details/TransferDetails.jsx
@@ -3,21 +3,18 @@ import { commonStyles as s } from './commonStyles';
 
 const TransferDetails = ({ details }) => {
   const isHandOff = !!details.handOffFlow;
-  
   return (
-    <>
-      <div style={s.section}>
-        <div style={s.sectionTitle}>{isHandOff ? 'Flow Handoff' : 'Blind Transfer'}</div>
-        {isHandOff ? (
-            <>
-            <div style={s.row}><span style={s.label}>Target Flow</span><span style={s.value}>{details.handOffFlow?.handOffToName}</span></div>
-            <div style={s.row}><span style={s.label}>Flow ID</span><span style={s.value}>{details.handOffFlow?.handOffTo}</span></div>
-            </>
-        ) : (
-            <div style={s.row}><span style={s.label}>Destination Number</span><span style={s.value}>{details.transfertodn || details['transfertodn:name']}</span></div>
-        )}
-      </div>
-    </>
+    <div style={s.section}>
+      <div style={s.sectionTitle}>{isHandOff ? 'Flow Handoff' : 'Blind Transfer'}</div>
+      {isHandOff ? (
+          <>
+          <div style={s.row}><span style={s.label}>Flow Name</span><span style={s.value}>{details.handOffFlow?.handOffToName}</span></div>
+          <div style={s.row}><span style={s.label}>Flow ID</span><span style={s.value}>{details.handOffFlow?.handOffTo}</span></div>
+          </>
+      ) : (
+          <div style={s.row}><span style={s.label}>Destination</span><span style={s.value}>{details.transfertodn || details['transfertodn:name']}</span></div>
+      )}
+    </div>
   );
 };
 export default TransferDetails;
