8fcc9f9a9e0d2272d6c0314f50fcb29a15e77c16
griffindunn <griffindunn@gmail.com>
2026-02-13 13:42:14 -0800

    Update BusinessHoursDetails.jsx

--- src/components/details/BusinessHoursDetails.jsx
+++ src/components/details/BusinessHoursDetails.jsx
@@ -3,15 +3,9 @@ import { commonStyles as s } from './commonStyles';
 
 const BusinessHoursDetails = ({ details }) => (
   <div style={s.section}>
-    <div style={s.sectionTitle}>Schedule Configuration</div>
-    <div style={s.row}>
-       <span style={s.label}>Schedule Name</span>
-       <span style={s.value}>{details['businessHoursId:name'] || details.businessHoursId}</span>
-    </div>
-    <div style={s.row}>
-       <span style={s.label}>Input Mode</span>
-       <span style={s.value}>{details.businessHoursRadioGroup === 'staticBusinessHours' ? 'Static Schedule' : 'Variable'}</span>
-    </div>
+    <div style={s.sectionTitle}>Schedule</div>
+    <div style={s.row}><span style={s.label}>Name</span><span style={s.value}>{details['businessHoursId:name'] || details.businessHoursId}</span></div>
+    <div style={s.row}><span style={s.label}>Mode</span><span style={s.value}>{details.businessHoursRadioGroup}</span></div>
   </div>
 );
 export default BusinessHoursDetails;
