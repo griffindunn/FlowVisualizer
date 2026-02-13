e0513aff45a700ac074c3921c4cbfcc9a7a54828
griffindunn <griffindunn@gmail.com>
2026-02-13 13:52:38 -0800

    Update QueueContactNode.jsx

--- src/components/nodes/QueueContactNode.jsx
+++ src/components/nodes/QueueContactNode.jsx
@@ -1,23 +1,3 @@
+import SimpleActionNode from './SimpleActionNode';
 import React, { memo } from 'react';
-import { Handle, Position } from 'reactflow';
-import BaseNodeShell from './BaseNodeShell';
-import { nodeRowStyles as row } from './nodeRowStyles';
-
-const QueueContactNode = ({ data, selected }) => {
-  return (
-    <BaseNodeShell data={data} selected={selected}>
-      <div style={row.container}>
-         <Handle type="source" position={Position.Right} id="default" style={row.handleRight} />
-      </div>
-      <div style={row.divider} />
-      {/* Queue Contact typically has Failure and Error */}
-      {['failure', 'error'].map(key => (
-        <div key={key} style={row.errorContainer}>
-           <span style={row.errorLabel}>{key}</span>
-           <Handle type="source" position={Position.Right} id={key} style={row.handleError} />
-        </div>
-      ))}
-    </BaseNodeShell>
-  );
-};
-export default memo(QueueContactNode);
+export default memo((props) => <SimpleActionNode {...props} exits={['failure', 'error']} />);
