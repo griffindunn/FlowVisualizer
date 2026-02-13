3f8b3ac5283d4b32bf96a110c73f020883199f9b
griffindunn <griffindunn@gmail.com>
2026-02-13 13:41:38 -0800

    Update SetVariableDetails.jsx

--- src/components/details/SetVariableDetails.jsx
+++ src/components/details/SetVariableDetails.jsx
@@ -3,7 +3,6 @@ import { commonStyles as s } from './commonStyles';
 
 const SetVariableDetails = ({ details }) => {
   const vars = details.setVariablesArray || [];
-
   return (
     <div style={s.section}>
       <div style={s.sectionTitle}>Variables Configured ({vars.length})</div>
@@ -11,18 +10,11 @@ const SetVariableDetails = ({ details }) => {
       
       {vars.map((v, i) => (
         <div key={i} style={{marginBottom: '10px', paddingBottom: '10px', borderBottom: '1px dashed #eee'}}>
-           <div style={s.row}>
-             <span style={s.label}>Target Variable</span>
-             <span style={{fontWeight: 'bold', color: '#005073'}}>{v.srcVariable || v.name}</span>
-           </div>
-           <div style={s.row}>
-             <span style={s.label}>Value</span>
-             <span style={s.value}>{v.literal || v.value}</span>
-           </div>
+           <div style={s.row}><span style={s.label}>Variable</span><span style={{fontWeight: 'bold', color: '#005073'}}>{v.srcVariable || v.name}</span></div>
+           <div style={s.row}><span style={s.label}>Value</span><span style={s.value}>{v.literal || v.value}</span></div>
         </div>
       ))}
     </div>
   );
 };
-
 export default SetVariableDetails;
